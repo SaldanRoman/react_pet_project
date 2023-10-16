ccfd307b49516f5b001a4159fbeb78e17c661081
Roman Saldan <saldanroman@gmail.com>
2023-10-16 03:54:16 +0300

    used Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './App.css';
 import { ProductCard } from './components/product-card/product-card';
 import { Loading } from './components/shared/loading';
@@ -7,14 +7,14 @@ import { useProducts } from './hooks/product';
 import { Modal } from './components/shared/modal';
 import { CreateProduct } from './components/product-card/createProduct';
 import { Product } from './models/product.model';
+import { ModalContext } from './components/context/modalContext';
 
 function App() {
   const {products, isLoading, error, addProduct} = useProducts();
-  const [modal, setModal] = useState(true)
-
+const {modal, open, close} = useContext(ModalContext);
 const createHandler = (product:Product) => { 
   addProduct(product) ;
-  setModal(false)
+ close();
 }
 
   return (
@@ -22,8 +22,8 @@ const createHandler = (product:Product) => {
       {isLoading && <Loading></Loading>}
       {error && <ErrorMessage error={error}></ErrorMessage>}
       {products.map((product, index)=> <ProductCard product={product} key={`id-prod${index}`}></ProductCard>)}
-
-      {modal && <Modal onClose={()=> setModal(false)} title='Create product'> <CreateProduct onCreate={createHandler}></CreateProduct> </Modal>}
+      <button className='fixed bottom-5 right-5 rounded-full bg-yellow-400 text-white text-2xl px-4 py-2"' onClick={open}>+</button>
+      {modal && <Modal onClose={close} title='Create product'> <CreateProduct onCreate={createHandler}></CreateProduct> </Modal>}
     </div>
   );
 }
---
+++ src/components/context/modalContext.tsx
@@ -0,0 +1,22 @@
+import { ReactNode, createContext, useState } from "react";
+
+interface Modal {
+    modal: boolean
+    open: () => void
+    close: () => void
+}
+export const ModalContext = createContext<Modal>({
+    modal: false,
+    open: () => {},
+    close: () => {}
+})
+
+export const ModalState = ({children}: {children:ReactNode}) => {
+    const [modal, setModal] = useState(false); 
+    const open  = () => setModal(true);
+    const close = () => setModal(false);
+    
+    return (    
+    <ModalContext.Provider value={{modal, open, close}}> { children }</ModalContext.Provider>
+    )
+}
